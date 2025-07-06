d048f952d74a7dd9eb75641fc72361eb4ce53775
dvodnenko <thekovalskii@gmail.com>
2025-07-06 20:06:02 +0300

    feat(test): add sessions logic

--- src/raww/data_v2.py
+++ src/raww/data_v2.py
@@ -4,6 +4,8 @@ trying to switch functional aproach to OOP
 
 from pathlib import Path
 import pickle
+from datetime import datetime, date
+
 
 from .views import format_work_time_info
 
@@ -27,20 +29,42 @@ class TotalWorkTime:
     @property
     def infostr(self) -> str:
         return format_work_time_info(self.hours, self.minutes, self.seconds)
+class Session:
+    def __init__(
+            self, 
+            tags: list[str], 
+            start_datetime: datetime, 
+            end_datetime: datetime, 
+            breaks: int
+    ):
+        self.tags = tags
+        self.start_datetime = start_datetime
+        self.end_datetime = end_datetime
+        self.breaks = breaks
 
+    @property
+    def total(self):
+        diff = (self.end_datetime - self.start_datetime).seconds - self.breaks
 
-worktime = TotalWorkTime(hours=0, minutes=0, seconds=0)
-print(worktime.infostr)
+        hours = diff // 3600
+        diff -= hours*3600
+        minutes = diff // 60
+        diff -= minutes * 60
+        seconds = diff
+
+        return TotalWorkTime(hours=hours, minutes=minutes, seconds=seconds)
 
 
 class Data:
 
     __ts_df_title = 'tags.pickle'
+    __ss_df_title = 'sessions.pickle'
     
     def __init__(self, dir: Path):
         self.dir = dir
 
         self.__tags_path = dir / self.__ts_df_title
+        self.__sessions_path = dir / self.__ss_df_title
 
     @property
     def tags(self) -> list[str]:
@@ -55,6 +79,21 @@ class Data:
 
         with open(self.__tags_path, 'wb') as file:
             pickle.dump(newtags, file)
+        return None
+
+    @property
+    def sessions(self) -> list[Session]:
+        create_file_if_not_exists(self.__sessions_path, [])
+
+        with open(self.__sessions_path, 'rb') as file:
+            sessions: list[str] = pickle.load(file)
+        return sessions
+    @sessions.setter
+    def sessions(self, newsessions):
+        create_file_if_not_exists(self.__sessions_path, [])
+
+        with open(self.__sessions_path, 'wb') as file:
+            pickle.dump(newsessions, file)
         return None
 
 
