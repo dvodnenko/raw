fbf18b020c27bb0f397cd5d15fee158b687ac950
dvodnenko <thekovalskii@gmail.com>
2025-07-06 20:46:07 +0300

    feat(test): add active session logic

--- src/raww/data_v2.py
+++ src/raww/data_v2.py
@@ -3,7 +3,7 @@ trying to switch functional aproach to OOP
 '''
 
 from pathlib import Path
-import pickle
+import pickle, time
 from datetime import datetime, date
 
 
@@ -53,18 +53,53 @@ class Session:
         seconds = diff
 
         return TotalWorkTime(hours=hours, minutes=minutes, seconds=seconds)
-
+class ActiveSession:
+    def __init__(
+            self, 
+            tags: list[str], 
+            start_datetime: datetime,
+            breaks: int
+    ):
+        self.tags = tags
+        self.start_datetime = start_datetime
+        self.breaks = breaks
+    @staticmethod
+    def _begin(tags: list, path: Path): # !underhood method!
+        active_session = ActiveSession(tags, datetime.now(), 0)
+        with open(path, 'wb') as file:
+            pickle.dump(active_session, file)
+        return active_session
+    @staticmethod
+    def _finish(path: Path): # !underhood method!
+        with open(path, 'rb') as file:
+            active_session: ActiveSession = pickle.load(file)
+        with open(path, 'wb') as file:
+            pickle.dump(None, file)
+        return Session(
+            active_session.tags, 
+            active_session.start_datetime, 
+            datetime.now(),
+            active_session.breaks
+        )
+        
 
 class Data:
 
     __ts_df_title = 'tags.pickle'
     __ss_df_title = 'sessions.pickle'
+    __as_df_title = 'active_session.pickle'
     
     def __init__(self, dir: Path):
         self.dir = dir
 
         self.__tags_path = dir / self.__ts_df_title
         self.__sessions_path = dir / self.__ss_df_title
+        self.__as_path = self.dir / self.__as_df_title
+    def begin_session(self, tags: list[str]) -> ActiveSession:
+        return ActiveSession._begin(tags, path=self.__as_path)
+    def finish_session(self) -> Session:
+        return ActiveSession._finish(self.__as_path)
+
 
     @property
     def tags(self) -> list[str]:
@@ -94,6 +129,21 @@ class Data:
 
         with open(self.__sessions_path, 'wb') as file:
             pickle.dump(newsessions, file)
+        return None
+
+    @property
+    def active_session(self) -> ActiveSession | None:
+        create_file_if_not_exists(self.__as_path, None)
+
+        with open(self.__as_path, 'rb') as file:
+            active_session: ActiveSession | None = pickle.load(file)
+        return active_session
+    @active_session.setter
+    def active_session(self, activesession):
+        create_file_if_not_exists(self.__as_path, None)
+
+        with open(self.__as_path, 'wb') as file:
+            pickle.dump(activesession, file)
         return None
 
 
