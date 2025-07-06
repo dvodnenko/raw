1de656d722b87e2ba4e67115b8aa1e4c5763a555
dvodnenko <thekovalskii@gmail.com>
2025-07-06 19:42:22 +0300

    feat(test): add total work time class
    if i see that it is not needed - i'll remove it; but rn it seems extremly useful

--- src/raww/data_v2.py
+++ src/raww/data_v2.py
@@ -5,6 +5,8 @@ trying to switch functional aproach to OOP
 from pathlib import Path
 import pickle
 
+from .views import format_work_time_info
+
 
 def create_file_if_not_exists(path: Path, default_content):
     if path.exists():
@@ -14,6 +16,21 @@ def create_file_if_not_exists(path: Path, default_content):
     path.touch()
     with open(path, 'wb') as file:
         pickle.dump(default_content, file)
+
+
+class TotalWorkTime:
+    def __init__(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    @property
+    def infostr(self) -> str:
+        return format_work_time_info(self.hours, self.minutes, self.seconds)
+
+
+worktime = TotalWorkTime(hours=0, minutes=0, seconds=0)
+print(worktime.infostr)
 
 
 class Data:
@@ -26,7 +43,7 @@ class Data:
         self.__tags_path = dir / self.__ts_df_title
 
     @property
-    def tags(self):
+    def tags(self) -> list[str]:
         create_file_if_not_exists(self.__tags_path, [])
 
         with open(self.__tags_path, 'rb') as file:
