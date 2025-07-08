aabd603da0351236b373b22d981f3d5b5b3b0461
dvodnenko <thekovalskii@gmail.com>
2025-07-08 12:17:50 +0300

    feat: ability to create many tags at one moment

--- src/raww/tags.py
+++ src/raww/tags.py
@@ -4,25 +4,29 @@ from .data import Data
 
 
 @click.command('tags')
-@click.option('--new')
+@click.option('--new', default='')
 @click.pass_context
 def tag(
     ctx: click.Context,
     new: str
 ):
 
+    if new == '':
+        new = []
+    else:
+        new = new.split(',')
+
     raww_directory = ctx.obj['raww_directory']
     data = Data(raww_directory)
     mytags = data.tags
 
     if new:
-        if new in mytags:
-            click.echo(f'🦇 tag {new} already exists')
-            exit(1)
-        else:
-            data.tags = [*data.tags, new]
-            click.echo(f'🦇 new tag - {new}')
-            exit(0)
+        for newtag in new:
+            if newtag in mytags:
+                click.echo(f'🦇 tag {newtag} already exists')
+            else:
+                data.tags = [*data.tags, newtag]
+                click.echo(f'🦇 new tag - {newtag}')
     else:
         if mytags == []:
             click.echo('🦇 your tag list is empty right now')
